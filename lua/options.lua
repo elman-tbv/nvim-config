f33fc13f545a8c794f71bf5a1414c687405c8873
elman <elman.teyubov@gmail.com>
2025-05-09 19:12:41 +0400

    set vim options

---
+++ lua/options.lua
@@ -0,0 +1,18 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.colorcolumn = "80"
+vim.opt.signcolumn = auto
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 0
+-- vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+
+vim.opt.mouse = 'a'
+vim.opt.termguicolors = true
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
