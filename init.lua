5b060d11fe66ced02e2977898264dd53113aef88
elman <elman.teyubov@gmail.com>
2025-05-10 00:41:36 +0400

    add complitetion plugin

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 require "options"
 require "lazysetup"
 
+require "plugins.cmp"
 require "plugins.lsp"
 
 vim.cmd.colorscheme("gruvbox")
--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -38,4 +38,10 @@ return {
   "williamboman/mason.nvim",
   "williamboman/mason-lspconfig.nvim",
   "neovim/nvim-lspconfig",
+
+  -- complitetion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {'hrsh7th/cmp-nvim-lsp'},
+  },
 }
