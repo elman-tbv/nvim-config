3734541fb84a5369b0bb2e83a3196474966f777a
elman <elman.teyubov@gmail.com>
2025-05-10 00:41:36 +0400

    add complitetion plugin

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -39,4 +39,10 @@ return {
   "williamboman/mason-lspconfig.nvim",
   "neovim/nvim-lspconfig",
   "folke/neodev.nvim",  -- configures lua-lsp for Neovim
+
+  -- complitetion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {'hrsh7th/cmp-nvim-lsp'},
+  },
 }
